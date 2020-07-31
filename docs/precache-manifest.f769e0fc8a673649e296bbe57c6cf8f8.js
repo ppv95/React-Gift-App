d70d6f971439280db2971a42a010f202e23bb245
Paul Peña Vasquez <ppv2118@gmail.com>
2020-07-30 21:06:44 -0400

    docs agregado

---
+++ docs/precache-manifest.f769e0fc8a673649e296bbe57c6cf8f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fc073188c4e6971e6413260ad8e43e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67d22ee48e4affc369c7",
+    "url": "/static/css/main.4a854879.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d22ee48e4affc369c7",
+    "url": "/static/js/main.6e8176fe.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
